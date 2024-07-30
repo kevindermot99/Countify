c2433cfec91fca48a0beb8af3d1990082ab56140
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-30 09:59:59 +0200

    Tailwind init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
 
 function App() {
 
   return (
     <>
-    hhh
+      <HashRouter>
+        <Routes>
+          <Route path={`/`} element={<Home />} />
+        </Routes>
+      </HashRouter>
     </>
   )
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        ww
+    </div>
+  )
+}
+
+export default Home
