4b2491608ed9ddb2cf9cb550a91eaefe33631013
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-30 18:17:33 +0200

    features section complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ import { FiArrowRight } from "react-icons/fi";
 
 function Home() {
     return (
-        <div className="w-full h-svh flex flex-col items-start justify-start relative text-black">
+        <div className="w-full h-fit flex flex-col items-start justify-start relative text-white bg-black">
             {/* hero */}
             <div className="flex items-center justify-start flex-col bg-black text-white relative w-full h-fit">
                 {/* light */}
@@ -41,12 +41,12 @@ function Home() {
                     </div>
                 </div>
                 {/* visuals */}
-                <div className='w-full flex-1 flex items-start justify-between overflow-clip gap-10 pt-5 px-10 max-md:px-4 mt-5 z-20 max-w-[1500px] max-h-[550px] '>
+                <div className='w-full flex-1 flex items-center justify-between overflow-clip gap-10 pt-5 px-10 max-md:px-4 mt-5 z-20 max-w-[1500px] max-h-[550px] '>
                     <div className='min-w-[500px] max-md:w-[300px] h-full flex flex-col items-start justify-center gap-5'>
                         <h1 className='text-4xl font-medium'>Quick and free word counting for paragraphs, essays, and PDFs.</h1>
                         <p className='font-light text-white/80'>Just paste your text or upload PDFs to get instant counts of words, characters, lines, sentences, and spaces. Quick and accurate results only!</p>
                         <Link to={`/app/counter`} type="reset" className="h-[44px] select-none min-w-[140px] rounded-xl text-base font-semibold px-5 flex items-center gap-2 bg-white transition active:scale-95 text-black">
-                            Open Countify 
+                            Open Countify
                             <FiArrowRight className='text-xl' />
                         </Link>
                     </div>
@@ -54,7 +54,10 @@ function Home() {
                 </div>
             </div>
             {/* features */}
-            <div></div>
+            <div className='w-full h-fit min-h-[400px] bg-stone-500/5 flex flex-col items-center justify-start py-20'>
+                <h1 className='text-3xl font-medium max-w-[440px] text-center'>Top Features You’ll get</h1>
+                
+            </div>
         </div>
     )
 }
