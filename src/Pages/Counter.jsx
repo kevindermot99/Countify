9ef11d55e39b0e0fa98f91365e9a9b9162d644ef
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-30 10:21:57 +0200

    logo init

--- src/Pages/Counter.jsx
+++ src/Pages/Counter.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SiPanasonic } from "react-icons/si";
+import { useLocation } from "react-router-dom";
+import logo from '../assets/logo.png'
 
 function Counter() {
   const [loading, setLoading] = useState(true);
+  const location = useLocation()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1600);
+  }, [location])
   return (
     <>
-    {loading && 
-      <div className="w-full h-svh flex items-center justify-center">
-        <span className="loader"></span>
-      </div>
-    }
+      {loading &&
+        <div className="w-full h-svh flex items-center justify-center">
+          <span className="loader"></span>
+        </div>
+      }
+      {!loading &&
+        <div className="w-full h-svh flex flex-col items-start justify-start">
+          {/* nav */}
+          <div className="min-h-[60px] w-full flex items-center justify-between px-10 py-5">
+            <div className="flex items-center justify-start gap-1">
+              <img src={logo} className="h-9" />
+              <p className="text-2xl font-semibold">Countify</p>
+            </div>
+            <div className="flex items-center justify-end">
+              
+            </div>
+          </div>
+        </div>
+      }
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        "main-color": "#648DE5",
+        "body-color-light": "#ffffff",
+        "body-color-dark": "#121212",
+        "border-lines-light": "#e1e1e1",
+        "border-lines-dark": "#1D1D1D",
+        "dark-text": "#2F333C",
+        "light-text": "#7d7d7d",
+      },
+    },
   },
   plugins: [],
-}
-
+};
