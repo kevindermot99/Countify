76cf806ee793687cdc098714897a7c052ab67147
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-01 08:25:50 +0200

    counter responsive

--- src/Pages/Counter.jsx
+++ src/Pages/Counter.jsx
@@ -91,7 +91,7 @@ function Counter() {
         <div className="w-full h-svh flex flex-col items-start justify-start text-black relative">
           {/* info fixed */}
           <div className={` w-full h-svh bg-body/95 backdrop-blur-md py-5 px-10 fixed top-0 right-0 ${showinfo ? 'transition duration-300 z-10' : 'translate-y-20 -z-10 opacity-0 pointer-events-none'}`}>
-            <button onClick={() => setShowInfo(false)} className="absolute top-7 right-10 bg-stone-200 rounded-full p-2">
+            <button onClick={() => setShowInfo(false)} className="absolute top-7 right-10 max-md:right-6 bg-stone-200 rounded-full p-2">
               <AiOutlineClose className="text-lg" />
             </button>
             <div className="flex-1 h-full flex-col flex items-center justify-center">
@@ -102,16 +102,16 @@ function Counter() {
           </div>
 
           {/* nav */}
-          <div className="min-h-[60px] w-full flex items-center justify-between px-10 py-5">
+          <div className="min-h-[60px] w-full flex items-center justify-between px-10 max-md:px-3 py-5">
             <a href={`/`} className="flex items-center justify-start gap-1">
               <img src={logo} className="h-9" />
-              <p className="text-2xl font-semibold">Countify</p>
+              <p className="text-2xl font-semibold max-md:text-xl">Countify</p>
             </a>
             <div className="flex items-center justify-end gap-5">
               <input id="pdf-upload" hidden type="file" accept="application/pdf" onChange={extractText} />
               <label htmlFor="pdf-upload" disabled={counting} className="text-sm flex items-center gap-1 py-1 rounded-md px-3 bg-stone-200 cursor-pointer transition active:scale-90">
                 <MdOutlineFileUpload className="text-2xl opacity-30" />
-                <p>Upload PDF</p>
+                <p className="max-md:hidden">Upload PDF</p>
               </label>
               <button onClick={() => setShowInfo(true)} className="text-xl cursor-pointer">
                 <BsInfoCircle />
@@ -119,33 +119,33 @@ function Counter() {
             </div>
           </div>
           {/* hero */}
-          <div className=" w-full h-fit flex flex-col items-center justify-start py-10 gap-3">
-            <h1 className="text-4xl font-semibold max-w-[500px] text-center">Count Words Fast, Free, and Effortlessly.</h1>
-            <p className="text-base text-black/70 max-w-[400px] text-center">Copy and paste your text/paragraphs in the textbox bellow or upload PDFs and click count button</p>
+          <div className=" w-full h-fit flex flex-col items-center justify-start py-10 max-md:py-16 gap-3 px-4">
+            <h1 className="text-4xl font-semibold max-w-[500px] text-center max-md:text-2xl">Count Words Fast, Free, and Effortlessly.</h1>
+            <p className="text-base text-black/70 max-w-[400px] text-center max-md:text-sm">Copy and paste your text/paragraphs in the textbox bellow or upload PDFs and click count button</p>
           </div>
           {/* input */}
-          <div className="w-full h-fit flex flex-col gap-5 items-center justify-start">
+          <div className="w-full h-fit flex flex-col max-md:flex-col-reverse gap-5 items-center justify-start">
             {/* result */}
-            <div id="result-section" className=" scroll-m-[50px] w-[95%] max-w-[1000px] min-h-[70px] bg-white rounded-xl flex items-center justify-center py-4 px-10">
-              <div className="w-full border-r-[1px] border-black/10 mr-7">
+            <div id="result-section" className=" scroll-m-[50px] w-[95%] max-w-[1000px] h-[70px] bg-white rounded-xl flex items-center justify-center max-md:flex-col max-md:px-6 max-md:h-fit py-4 px-10">
+              <div className="w-full border-r-[1px] border-black/10 mr-7 max-md:mr-0 max-md:border-b-[1px] max-md:mb-4 max-md:pb-3 max-md:border-r-transparent">
                 <h1> Words</h1>
                 <p className="text-xl">
                   {counting ? <span className="w-[80px] h-[24px] flex rounded-sm bg-gradient-to-tl from-stone-300/50 via-stone-200 to-stone-100 animate-pulse"></span> : <span>{wordCount}</span>}
                 </p>
               </div>
-              <div className="w-full border-r-[1px] border-black/10 mr-7">
+              <div className="w-full border-r-[1px] border-black/10 mr-7 max-md:mr-0 max-md:border-b-[1px] max-md:mb-4 max-md:pb-3 max-md:border-r-transparent">
                 <h1> Characters</h1>
                 <p className="text-xl">
                   {counting ? <span className="w-[80px] h-[24px] flex rounded-sm bg-gradient-to-tl from-stone-300/50 via-stone-200 to-stone-100 animate-pulse"></span> : <span>{characterCount}</span>}
                 </p>
               </div>
-              <div className="w-full border-r-[1px] border-black/10 mr-7">
+              <div className="w-full border-r-[1px] border-black/10 mr-7 max-md:mr-0 max-md:border-b-[1px] max-md:mb-4 max-md:pb-3 max-md:border-r-transparent">
                 <h1> Whitespaces</h1>
                 <p className="text-xl">
                   {counting ? <span className="w-[80px] h-[24px] flex rounded-sm bg-gradient-to-tl from-stone-300/50 via-stone-200 to-stone-100 animate-pulse"></span> : <span>{whiteSpaceCount}</span>}
                 </p>
               </div>
-              <div className="w-full border-r-[1px] border-black/10 mr-7">
+              <div className="w-full border-r-[1px] border-black/10 mr-7 max-md:mr-0 max-md:border-b-[1px] max-md:mb-4 max-md:pb-3 max-md:border-r-transparent">
                 <h1> Sentences</h1>
                 <p className="text-xl">
                   {counting ? <span className="w-[80px] h-[24px] flex rounded-sm bg-gradient-to-tl from-stone-300/50 via-stone-200 to-stone-100 animate-pulse"></span> : <span>{sentencesCount}</span>}
@@ -160,9 +160,9 @@ function Counter() {
             </div>
             {/* input */}
             <form onSubmit={submitText} className="w-[95%] max-w-[1000px] mx-auto h-full flex items-start justify-start flex-col">
-              <textarea value={text} onChange={(e) => setText(e.target.value)} type="text" required className="w-full h-[400px] bg-white rounded-xl p-10 text-lg resize-none custom-scrollbar" placeholder="Paste your text here.." />
-              <div className="flex items-center justify-start mt-5 gap-5">
-                <button type="submit" className="h-[48px] select-none min-w-[140px] rounded-xl text-base font-semibold px-5 bg-black text-white transition active:scale-95">
+              <textarea value={text} onChange={(e) => setText(e.target.value)} type="text" required className="w-full h-[400px] bg-white rounded-xl p-10 max-md:p-5 text-lg max-md:text-sm text-justify max-md:pr-2 resize-none custom-scrollbar" placeholder="Paste your text here.." />
+              <div className="flex items-center justify-start mt-5 gap-5 max-md:gap-3 max-md:flex-col max-md:w-full">
+                <button type="submit" className="h-[45px] select-none min-w-[140px] max-md:w-full rounded-xl text-base font-semibold px-5 bg-black text-white transition active:scale-95">
                   {counting ? (
                     <div className="flex items-center justify-center gap-2">
                       <span className="button-loader"></span>
@@ -172,16 +172,17 @@ function Counter() {
                     <p>Count</p>
                   )}
                 </button>
-                <button onClick={clearEverything} type="reset" className="h-[48px] select-none min-w-[140px] rounded-xl text-base font-semibold px-5 bg-stone-300/60 hover:bg-stone-400/70 transition active:scale-95 text-black">
+                <button onClick={clearEverything} type="reset" className="h-[45px] select-none min-w-[140px] max-md:w-full rounded-xl text-base font-semibold px-5 bg-stone-300/60 hover:bg-stone-400/70 transition active:scale-95 text-black">
                   Clear
                 </button>
               </div>
             </form>
-            {/* footer */}
-            <div className="mt-5 text-sm py-3 text-black/40 flex items-center justify-center">
+            
+          </div>
+          {/* footer */}
+          <div className="mt-5 text-sm py-3 text-black/40 flex items-center justify-center flex-wrap text-center px-4 max-md:mt-0">
               Countify does not store your data. By using this tool, you agree to our <Link to={'/app/terms of service'} className="hover:underline text-black/70 pl-1">Terms of Service</Link>
             </div>
-          </div>
 
         </div>
       }
